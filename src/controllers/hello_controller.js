696330494ec4fc8d7b952bfa59c528b30bb68ddd
Jeremy Frank <jeremyjfrank@gmail.com>
2022-08-12 15:46:20 -0400

    Enable and disable hello controller button

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -1,21 +1,49 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['name']
+  static targets = ['input', 'button']
+  static values = {
+    'enabled': Boolean
+  }
   
   connect() {
     console.log('Hello controller connected', this.element)
+    this._checkValidity()
   }
-
+  
   disconnect() {
     console.log('Hello controller disconnected', this.element)
   }
-
+  
   greet() {
     console.log(`Hello, ${this.name}!`)
   }
+  
+  handleKeyup() {
+    this._checkValidity()
+  }
+
+  enabledValueChanged() {
+    if (this.enabledValue) {
+      this._enableButton()
+    } else {
+      this._disableButton()
+    }
+  }
+
+  _checkValidity() {
+    this.enabledValue = this.inputTarget.value.length > 0
+  }
+
+  _enableButton() {
+    this.buttonTarget.removeAttribute("disabled")
+  }
+  
+  _disableButton() {
+    this.buttonTarget.setAttribute("disabled", "")
+  }
 
   get name() {
-    return this.nameTarget.value
+    return this.inputTarget.value
   }
 }
